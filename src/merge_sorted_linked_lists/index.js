fe6e11a0ea6660a0041257e3b83467a3a94af39b
Thai Tran <thai.tran@tcnbroadcasting.com>
2018-11-20 00:06:55 -0700

    merge_sorted_linked_lists

---
+++ src/merge_sorted_linked_lists/index.js
@@ -0,0 +1,57 @@
+const isEmpty = function(node) {
+	if (node.val === undefined) return true;
+	return false;
+}
+
+// Optimize this whole method
+const mergeTwoLists = function(l1, l2) { 
+	let headA = isEmpty(l1) ? null : l1;
+	let headB = isEmpty(l2) ? null : l2;
+	let res = new ListNode();
+	let tail; // try using removing the tail in the removeTail method rewrite
+	const baseNode = res;
+
+	function addToRes(val) { // addToRes only concerns the res
+		res.val = val;
+		res.next = new ListNode() // when will this list end?
+		tail = res;
+		res = res.next;
+	}
+
+	function removeTail() {
+		if (baseNode.val === undefined) return;
+		tail.next = null;
+		// let newHead = baseNode;
+		// while(newHead.next) {
+		// 	if (newHead.next.val === undefined) {
+		// 		newHead.next = null;
+		// 		break;
+		// 	}
+		// 	newHead = newHead.next;
+		// }
+	}
+	
+	while(headA !== null || headB !== null) { 
+		if (headA != null && headB == null) {
+			addToRes(headA.val)
+			headA = headA.next;
+		} else if (headA == null && headB != null) {
+			addToRes(headB.val)
+			headB = headB.next;
+		} else if (headA.val == headB.val) {
+			addToRes(headA.val);
+			addToRes(headB.val);
+			headA = headA.next;
+			headB = headB.next;
+		} else if (headA.val < headB.val) {
+			addToRes(headA.val)
+			headA = headA.next;
+		} else if (headA.val > headB.val) {
+			addToRes(headB.val)
+			headB = headB.next;
+		}
+	}
+
+	removeTail();
+	return baseNode.val === undefined ? null : baseNode;
+}
---
+++ src/merge_sorted_linked_lists/index.spec.js
@@ -0,0 +1,20 @@
+const testCase = require('assert');
+const mocha = require('mocha');
+const describe = mocha.describe;
+const mergeToLists = require('./index.js');
+
+// Definition for singly-linked list.
+function ListNode(val) {
+	this.val = val;
+	this.next = null;
+}}
+
+describe('mergeToLists', () => {
+  let firstList;
+  let secondList;
+  let result;
+  it('should null when both lists are empty', () => { 
+    result = mergeTwoLists(new ListNode(), new ListNode());
+    assert.equal(undefined, result); 
+  });
+}); 
